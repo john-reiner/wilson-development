aee52c062db49936927b3f92126640cf8c036e65
jareiner90 <johnreiner1990@gmail.com>
2020-07-02 13:42:12 -0400

    converted TodaysResources to a functional component

--- my-goal-app/src/components/TodaysResources.js
+++ my-goal-app/src/components/TodaysResources.js
@@ -3,24 +3,20 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 import CarouselResource from './CarouselResource';
 
-export default class TodaysResources extends Component {
+export default function TodaysResources(props) {
 
-    renderResources = () => {
-        return this.props.resources.map(resource => {
+    const renderResources = () => {
+        return props.resources.map(resource => {
             return <CarouselResource name={resource.name} url={resource.url}/>
         })
     }
 
-    render() {
         
         return (
         <div>
             <Carousel showThumbs={false} style={{width: '100%', height: '400px'}}>
-                {this.renderResources()}
+                {renderResources()}
             </Carousel>
         </div> 
     )
-    }
-
-
 }        
